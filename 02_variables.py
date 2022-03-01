5b324aa16a6fff4d7a47fb8b96d58448888e8864
Nishil1 <bharadwajn1136@masseyhigh.school.nz>
2022-03-01 18:08:17 +1300

    Update 02_variables.py

--- 02_variables.py
+++ 02_variables.py
@@ -1,13 +1,12 @@
 # code to learn about variables
 
 # variables can be a string
-
 greeting = "Hello World"
 
 # they can also be numbers
-
 fav_num = 7
 
-# this variable h as a problem
+# below is a constant (the name is in uppercase)
+PI = 3.14 
 
-my name = "Nishil"
+num_2 = 2
