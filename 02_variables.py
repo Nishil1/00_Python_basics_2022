d4fb52a4ce91f88d43ca73c38eb45f125912dbd3
Nishil1 <bharadwajn1136@masseyhigh.school.nz>
2022-03-01 17:52:19 +1300

    Create 02_variables.py

---
+++ 02_variables.py
@@ -0,0 +1,13 @@
+# code to learn about variables
+
+# variables can be a string
+
+greeting = "Hello World"
+
+# they can also be numbers
+
+fav_num = 7
+
+# this variable h as a problem
+
+my name = "Nishil"
