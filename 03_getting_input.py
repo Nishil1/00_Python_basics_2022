4df684fdbadac4bf8cc034c71dd777f3f4bdab54
Nishil1 <bharadwajn1136@masseyhigh.school.nz>
2022-03-01 18:57:51 +1300

    Create 03_getting_input.py

---
+++ 03_getting_input.py
@@ -0,0 +1,17 @@
+# ask the user for thier name
+username = input("Whats your name?\n")
+# ask the user for thier favroite integer
+fav_num = int(input("Whats your favroite number?\n"))
+# double, halve and square thier number
+double = fav_num * 2
+half = fav_num/2
+squared = fav_num * fav_num
+# greet the user
+print("Hi {} \nYour favroite number is {}".format(username, fav_num))
+# output the results of doubling, halving
+# and squaring thier favroite number
+print()
+print()
+print("Doubling your favroite number is = ", double)
+print("Halving your favroite number is = ",half)
+print("Squaring your favroite nnumber is = ", squared)
